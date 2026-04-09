6664c4d79fe88094559db02437df6a626281a7cb
Maab3li <maab3li555@gmail.com>
2026-04-09 03:09:27 -0700

    fix:updated images srcs in getWeatherIcon hook to make it appear on deployed version

--- src/hooks/getWeatherIcon.tsx
+++ src/hooks/getWeatherIcon.tsx
@@ -12,30 +12,30 @@ export const getWeatherIcon = (weatherCode:Array<number|string>=[]) => {
         try {
             for(let weaCode=0;weaCode<weatherCode.length;weaCode++) {
                 if(weatherCode[weaCode] == 0 || weatherCode[weaCode] == 1) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-sunny.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-sunny.webp');
                 }
                 else if(weatherCode[weaCode] == 2) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-partly-cloudy.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-partly-cloudy.webp');
                 }
                 else if(weatherCode[weaCode] == 3) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-overcast.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-overcast.webp');
                 }
                 else if(weatherCode[weaCode] == 45 || weatherCode[weaCode] == 48) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-fog.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-fog.webp');
                 }
                 else if(weatherCode[weaCode] == 51 || weatherCode[weaCode] == 53 || weatherCode[weaCode] == 55) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-drizzle.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-drizzle.webp');
                 }
                 else if(weatherCode[weaCode] == 61 || weatherCode[weaCode] == 63 || weatherCode[weaCode] == 65 || weatherCode[weaCode] == 80 || weatherCode[weaCode] == 81 || weatherCode[weaCode] == 82) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-rain.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-rain.webp');
                 }
 
                 else if(weatherCode[weaCode] == 71 || weatherCode[weaCode] == 73 || weatherCode[weaCode] == 75 || weatherCode[weaCode] == 77 || weatherCode[weaCode] == 85 || weatherCode[weaCode] == 86) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-snow.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-snow.webp');
                 }
 
                 else if(weatherCode[weaCode] == 95 || weatherCode[weaCode] == 96 || weatherCode[weaCode] == 99) {
-                    weatherIcons[weaCode] = ('/weather-app/src/assets/images/icon-storm.webp');
+                    weatherIcons[weaCode] = ('/weather-app/tree/master/src/assets/images/icon-storm.webp');
                 }
             }
             return weatherIcons
